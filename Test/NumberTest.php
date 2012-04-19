4cb909a5a6ff72dd605ac33d7055050059956998
Joe Lencioni <jlencion@gustavus.edu>
2012-04-19 08:24:05 -0500

    Simplified quantity tests

--- Test/NumberTest.php
+++ Test/NumberTest.php
@@ -37,133 +37,75 @@ class NumberTest extends \Gustavus\Test\Test
   }
 
   /**
-   * function to test quantity from an array
-   * @param  array  $array
-   * @return void
+   * @test
+   * @dataProvider QuantityData
    */
-  public function testQuantityFromArray(array $array = array())
+  public function Quantity($expected, $value, $singular, $plural, $showCount = 'all')
   {
-    foreach ($array as $key => $value) {
-      $number = new Utility\Number($value[0]);
-      $singular = (isset($value[1])) ? $value[1] : '';
-      $plural = (isset($value[2])) ? $value[2] : '';
-      $showCount = (isset($value[3])) ? $value[3] : 'all';
-      $this->assertSame($key, $number->quantity($singular, $plural, $showCount));
-    }
+    $this->number->setValue($value);
+    $this->assertSame($expected, $this->number->quantity($singular, $plural, $showCount));
   }
 
-  /**
-   * @test
-   */
-  public function QuantityWithUnspecifiedShowcount()
+  public static function QuantityData()
   {
-    $array = array(
-      '-10,000.15 tests' => array(-10000.15, 'test', 'tests'),
-      '-10,000.1 tests' => array(-10000.1, 'test', 'tests'),
-      '-10,000 tests' => array(-10000, 'test', 'tests'),
-      '-2 tests' => array(-2, 'test', 'tests'),
-      '-1.15 tests' => array(-1.15, 'test', 'tests'),
-      '-1.1 tests' => array(-1.1, 'test', 'tests'),
-      '-1 tests' => array(-1, 'test', 'tests'),
+    return array(
+      array('-10,000.15 tests', -10000.15, 'test', 'tests'),
+      array('-10,000.1 tests', -10000.1, 'test', 'tests'),
+      array('-10,000 tests', -10000, 'test', 'tests'),
+      array('-2 tests', -2, 'test', 'tests'),
+      array('-1.15 tests', -1.15, 'test', 'tests'),
+      array('-1.1 tests', -1.1, 'test', 'tests'),
+      array('-1 tests', -1, 'test', 'tests'),
 
-      '0 tests' => array(0, 'test', 'tests'),
+      array('0 tests', 0, 'test', 'tests'),
 
-      '1 test' => array(1, 'test', 'tests'),
-      '1.1 tests' => array(1.1, 'test', 'tests'),
-      '1.15 tests' => array(1.15, 'test', 'tests'),
-      '2 tests' => array(2, 'test', 'tests'),
-      '10,000 tests' => array(10000, 'test', 'tests'),
-      '10,000.1 tests' => array(10000.1, 'test', 'tests'),
-      '10,000.15 tests' => array(10000.15, 'test', 'tests'),
+      array('1 test', 1, 'test', 'tests'),
+      array('1.1 tests', 1.1, 'test', 'tests'),
+      array('1.15 tests', 1.15, 'test', 'tests'),
+      array('2 tests', 2, 'test', 'tests'),
+      array('10,000 tests', 10000, 'test', 'tests'),
+      array('10,000.1 tests', 10000.1, 'test', 'tests'),
+      array('10,000.15 tests', 10000.15, 'test', 'tests'),
+
+      // with plural showcount
+      array('-1 tests', -1, 'test', 'tests', 'plural'),
+
+      array('0 tests', 0, 'test', 'tests', 'plural'),
+
+      array('test', 1, 'test', 'tests', 'plural'),
+
+      array('1.1 tests', 1.1, 'test', 'tests', 'plural'),
+
+      // with none showcount
+      array('tests', -1, 'test', 'tests', 'none'),
+      array('tests', 0, 'test', 'tests', 'none'),
+      array('test', 1, 'test', 'tests', 'none'),
+      array('tests', 1.1, 'test', 'tests', 'none'),
+
+      // with all showcount
+      array('-1 tests', -1, 'test', 'tests', 'all'),
+      array('0 tests', 0, 'test', 'tests', 'all'),
+      array('1 test', 1, 'test', 'tests', 'all'),
+      array('1.1 tests', 1.1, 'test', 'tests', 'all'),
+
+      // with true showcount
+      array('-1 tests', -1, 'test', 'tests', true),
+      array('0 tests', 0, 'test', 'tests', true),
+      array('1 test', 1, 'test', 'tests', true),
+      array('1.1 tests', 1.1, 'test', 'tests', true),
+
+      // with false showcount
+      array('tests', -1, 'test', 'tests', false),
+      array('tests', 0, 'test', 'tests', false),
+      array('test', 1, 'test', 'tests', false),
+      array('tests', 1.1, 'test', 'tests', false),
+
+      // weird showcount
+      array('-1 tests', -1, 'test', 'tests', 'arstarstarst'),
+      array('0 tests', 0, 'test', 'tests', 'arstarstarst'),
+      array('1 test', 1, 'test', 'tests', 'arstarstarst'),
+      array('1.1 tests', 1.1, 'test', 'tests', 'arstarstarst'),
     );
-    $this->testQuantityFromArray($array);
-  }
-
-  /**
-   * @test
-   */
-  public function QuantityWithPluralShowcount()
-  {
-    $array = array(
-      '-1 tests' => array(-1, 'test', 'tests', 'plural'),
-
-      '0 tests' => array(0, 'test', 'tests', 'plural'),
-
-      'test' => array(1, 'test', 'tests', 'plural'),
-
-      '1.1 tests' => array(1.1, 'test', 'tests', 'plural'),
-    );
-    $this->testQuantityFromArray($array);
-  }
-
-  /**
-   * @test
-   */
-  public function QuantityWithNoneShowcount()
-  {
-    $array = array(
-      'tests' => array(-1, 'test', 'tests', 'none'),
-      'tests' => array(0, 'test', 'tests', 'none'),
-      'test' => array(1, 'test', 'tests', 'none'),
-      'tests' => array(1.1, 'test', 'tests', 'none'),
-    );
-    $this->testQuantityFromArray($array);
-  }
-
-  /**
-   * @test
-   */
-  public function QuantityWithAllShowcount()
-  {
-    $array = array(
-      '-1 tests' => array(-1, 'test', 'tests', 'all'),
-      '0 tests' => array(0, 'test', 'tests', 'all'),
-      '1 test' => array(1, 'test', 'tests', 'all'),
-      '1.1 tests' => array(1.1, 'test', 'tests', 'all'),
-    );
-    $this->testQuantityFromArray($array);
-  }
-
-  /**
-   * @test
-   */
-  public function QuantityWithTrueShowcount()
-  {
-    $array = array(
-      '-1 tests' => array(-1, 'test', 'tests', true),
-      '0 tests' => array(0, 'test', 'tests', true),
-      '1 test' => array(1, 'test', 'tests', true),
-      '1.1 tests' => array(1.1, 'test', 'tests', true),
-    );
-    $this->testQuantityFromArray($array);
-  }
-
-  /**
-   * @test
-   */
-  public function QuantityWithFalseShowcount()
-  {
-    $array = array(
-      'tests' => array(-1, 'test', 'tests', false),
-      'tests' => array(0, 'test', 'tests', false),
-      'test' => array(1, 'test', 'tests', false),
-      'tests' => array(1.1, 'test', 'tests', false),
-    );
-    $this->testQuantityFromArray($array);
-  }
-
-  /**
-   * @test
-   */
-  public function QuantityWithWeirdShowcount()
-  {
-    $array = array(
-      '-1 tests' => array(-1, 'test', 'tests', 'arstarstarst'),
-      '0 tests' => array(0, 'test', 'tests', 'arstarstarst'),
-      '1 test' => array(1, 'test', 'tests', 'arstarstarst'),
-      '1.1 tests' => array(1.1, 'test', 'tests', 'arstarstarst'),
-    );
-    $this->testQuantityFromArray($array);
   }
 
 }
