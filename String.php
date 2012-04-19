bccc0bd7b7cdef87d680b047fcd25a15c1d04728
Joe Lencioni <jlencion@gustavus.edu>
2012-04-19 13:08:05 -0500

    Started String class

---
+++ String.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Utility
+ */
+namespace Gustavus\Utility;
+use \Format;
+
+/**
+ * Object for working with Strings
+ *
+ * @package Utility
+ */
+class String extends Base
+{
+
+}
---
+++ Test/StringTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package Utility
+ * @subpackage Test
+ */
+
+namespace Gustavus\Utility\Test;
+use Gustavus\Utility,
+  Gustavus\Test\Test,
+  Gustavus\Test\Test\TestObject;
+
+/**
+ * @package Utility
+ * @subpackage Test
+ */
+class StringTest extends Test
+{
+  /**
+   * @var Utility\String
+   */
+  private $string;
+
+  /**
+   * sets up the object for each test
+   * @return void
+   */
+  public function setUp()
+  {
+    $this->string = new TestObject(new Utility\String());
+  }
+
+  /**
+   * destructs the object after each test
+   * @return void
+   */
+  public function tearDown()
+  {
+    unset($this->string);
+  }
+
+}
