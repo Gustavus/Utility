3a19fc95334ab95aab80cf53e9ab9f35bed6f0b4
Joe Lencioni <jlencion@gustavus.edu>
2012-04-19 14:25:02 -0500

    Added email() and widont()

--- String.php
+++ String.php
@@ -11,5 +11,112 @@ namespace Gustavus\Utility;
  */
 class String extends Base
 {
+  /**
+   * Function to overide abstract function in base to make sure the value is valid
+   *
+   * @param  mixed $value value passed into setValue()
+   * @return boolean
+   */
+  final protected function valueIsValid($value)
+  {
+    // DateTime will throw an exception if the value isn't supported. No need to do our own checks.
+    return true;
+  }
+
+  /**
+   * Fixes up sloppy URLs so they are correctly and uniformly formatted
+   *
+   * Usage:
+   * <code>
+   * $string = new String('gac.edu/test/');
+   * echo $string->url();
+   * // Outputs: http://gustavus.edu/test/
+   *
+   * $string = new String('google.com/testing/');
+   * echo $string->url();
+   * // Outputs: http://google.com/testing/
+   *
+   * $string = new String('www.gustavus.edu');
+   * echo $string->url();
+   * // Outputs: http://gustavus.edu
+   * </code>
+   *
+   * @return string
+   */
+  public function url()
+  {
+    $url = trim($this->value);
+
+    // A URL must be at least 3 characters because it needs to have a domain name, a dot, and a TLD
+    if (strlen($url) < 3) {
+      return '';
+    }
+
+    $url = preg_replace('`^(.*?):[/\\\]+`', '$1://', $url);
+
+    if (preg_match('`^(?:.*?)://`', $url) === 0) {
+      // URL does not begin with a protocol, so we default to http://
+      $url  = 'http://' . $url;
+    }
+
+    $url  = preg_replace('`(?<!homepages\.)(?:www\.)?g(?>ustavus|ac)\.edu`', 'gustavus.edu', $url);
+
+    return $url;
+  }
+
+  /**
+   * Fixes up sloppy e-mail addresses so they are correctly and uniformly formatted
+   *
+   * Usage:
+   * <code>
+   * $string = new String('jerry');
+   * echo $string->email();
+   * // Outputs: jerry@gustavus.edu
+   *
+   * $string = new String('jerry@gac.edu');
+   * echo $string->email();
+   * // Outputs: jerry@gustavus.edu
+   *
+   * $string = new String('jerry@gustavus.edu');
+   * echo $string->email();
+   * // Outputs: jerry@gustavus.edu
+   * </code>
+   *
+   * @return string
+   */
+  public function email()
+  {
+    $email = trim($this->value);
+
+    if (empty($email)) {
+      return '';
+    } else if (strpos($email, '@') === false) {
+      return "$email@gustavus.edu";
+    } else {
+      return str_replace('@gac.edu', '@gustavus.edu', $email);
+    }
+  }
+
+  /**
+   * Prevents widows in strings to be used in HTML and XHTML documents
+   *
+   * Example:
+   * <code>
+   * $string = new String('This is a test');
+   * echo $string->widont();
+   * // Outputs: "This is a&nbsp;test"
+   * </code>
+   *
+   * @param integer $lastWordMaxLength
+   * @return string String with widows removed
+   * @author Shaun Inman <io@shauninman.com>
+   * @link http://shauninman.com/archive/2007/01/03/widont_2_1_wordpress_plugin
+   */
+  public function widont($lastWordMaxLength = null)
+  {
+    assert('is_int($lastWordMaxLength) || is_null($lastWordMaxLength)');
+
+    return preg_replace("|([^\s])\s+([^\s]{1,$lastWordMaxLength})(</.+?>)*\s*$|", '$1&nbsp;$2$3', $this->value);
+  }
 
 }
--- Test/StringTest.php
+++ Test/StringTest.php
@@ -39,4 +39,168 @@ class StringTest extends Test
     unset($this->string);
   }
 
+  /**
+   * @test
+   * @dataProvider urlData
+   */
+  public function url($expected, $url)
+  {
+    $this->string->setValue($url);
+    $this->assertSame($expected, $this->string->url());
+  }
+
+  /**
+   * @return array
+   */
+  public static function urlData()
+  {
+    return array(
+      array('http://google.com', 'google.com'),
+      array('http://google.com', ' google.com '),
+      array('http://google.com', 'http://google.com'),
+      array('https://google.com', 'https://google.com'),
+      array('ftp://google.com', 'ftp://google.com'),
+
+      array('http://google.com/test', 'google.com/test'),
+      array('http://google.com/test', ' google.com/test '),
+      array('http://google.com/test', 'http://google.com/test'),
+      array('https://google.com/test', 'https://google.com/test'),
+      array('ftp://google.com/test', 'ftp://google.com/test'),
+
+      // Too short
+      array('', 'ab'),
+
+      // Gustavus URLs
+      array('http://gustavus.edu', 'gac.edu'),
+      array('http://gustavus.edu', ' gac.edu '),
+      array('http://gustavus.edu', 'http://gac.edu'),
+      array('https://gustavus.edu', 'https://gac.edu'),
+      array('ftp://gustavus.edu', 'ftp://gac.edu'),
+      array('http://gustavus.edu/test', 'gac.edu/test'),
+      array('http://gustavus.edu/test', 'http://gac.edu/test'),
+      array('https://gustavus.edu/test', 'https://gac.edu/test'),
+      array('ftp://gustavus.edu/test', 'ftp://gac.edu/test'),
+
+      array('http://gustavus.edu', 'gustavus.edu'),
+      array('http://gustavus.edu', ' gustavus.edu '),
+      array('http://gustavus.edu', 'http://gustavus.edu'),
+      array('https://gustavus.edu', 'https://gustavus.edu'),
+      array('ftp://gustavus.edu', 'ftp://gustavus.edu'),
+      array('http://gustavus.edu/test', 'gustavus.edu/test'),
+      array('http://gustavus.edu/test', 'http://gustavus.edu/test'),
+      array('https://gustavus.edu/test', 'https://gustavus.edu/test'),
+      array('ftp://gustavus.edu/test', 'ftp://gustavus.edu/test'),
+
+      // Homepages
+      array('http://homepages.gac.edu/~user/', 'http://homepages.gac.edu/~user/'),
+    );
+  }
+
+  /**
+   * @test
+   * @dataProvider emailData
+   */
+  public function email($expected, $email)
+  {
+    $this->string->setValue($email);
+    $this->assertSame($expected, $this->string->email());
+  }
+
+  /**
+   * @return array
+   */
+  public function emailData()
+  {
+    return array(
+      array('joe@google.com', 'joe@google.com'),
+      array('joe@google.com', ' joe@google.com'),
+      array('joe@google.com', 'joe@google.com '),
+      array('joe@google.com', ' joe@google.com '),
+
+      array('joe+test@google.com', 'joe+test@google.com'),
+      array('joe+test@google.com', ' joe+test@google.com'),
+      array('joe+test@google.com', 'joe+test@google.com '),
+      array('joe+test@google.com', ' joe+test@google.com '),
+
+      array('', ''),
+
+      array('joe@gustavus.edu', 'joe'),
+      array('joe@gustavus.edu', ' joe'),
+      array('joe@gustavus.edu', 'joe '),
+      array('joe@gustavus.edu', ' joe '),
+
+      array('joe@gustavus.edu', 'joe@gac.edu'),
+      array('joe@gustavus.edu', ' joe@gac.edu'),
+      array('joe@gustavus.edu', 'joe@gac.edu '),
+      array('joe@gustavus.edu', ' joe@gac.edu '),
+
+      array('joe+test@gustavus.edu', 'joe+test@gac.edu'),
+      array('joe+test@gustavus.edu', ' joe+test@gac.edu'),
+      array('joe+test@gustavus.edu', 'joe+test@gac.edu '),
+      array('joe+test@gustavus.edu', ' joe+test@gac.edu '),
+
+      array('joe@gustavus.edu', 'joe@gustavus.edu'),
+      array('joe@gustavus.edu', ' joe@gustavus.edu'),
+      array('joe@gustavus.edu', 'joe@gustavus.edu '),
+      array('joe@gustavus.edu', ' joe@gustavus.edu '),
+
+      array('joe+test@gustavus.edu', 'joe+test@gustavus.edu'),
+      array('joe+test@gustavus.edu', ' joe+test@gustavus.edu'),
+      array('joe+test@gustavus.edu', 'joe+test@gustavus.edu '),
+      array('joe+test@gustavus.edu', ' joe+test@gustavus.edu '),
+    );
+  }
+
+  /**
+   * @test
+   * @dataProvider widontData
+   */
+  public function widont($expected, $value, $lastWordMaxLength = null)
+  {
+    $this->string->setValue($value);
+    $this->assertSame($expected, $this->string->widont($lastWordMaxLength));
+  }
+
+  /**
+   * @return array
+   */
+  public static function widontData()
+  {
+    return array(
+      array('test ', 'test '),
+      array(' test', ' test'),
+      array(' test ', ' test '),
+
+      array('test&nbsp;test', 'test test'),
+      array('test&nbsp;test', 'test test '),
+      array(' test&nbsp;test', ' test test'),
+      array(' test&nbsp;test', ' test test '),
+
+      array('test&nbsp;test', 'test  test'),
+      array('test&nbsp;test', 'test  test '),
+      array(' test&nbsp;test', ' test  test'),
+      array(' test&nbsp;test', ' test  test '),
+
+      array('test test&nbsp;test', 'test test test'),
+      array('test test&nbsp;test', 'test test test '),
+      array(' test test&nbsp;test', ' test test test'),
+      array(' test test&nbsp;test', ' test test test '),
+
+      array('test test&nbsp;test', 'test test  test'),
+      array('test test&nbsp;test', 'test test  test '),
+      array(' test test&nbsp;test', ' test test  test'),
+      array(' test test&nbsp;test', ' test test  test '),
+
+      // With max length
+      array('test&nbsp;test', 'test test', 5),
+      array('test&nbsp;test', 'test test', 4),
+      array('test test', 'test test', 3),
+
+      // With max length and ending tags
+      array('<strong>test&nbsp;test</strong>', '<strong>test test</strong>', 5),
+      array('<strong>test&nbsp;test</strong>', '<strong>test test</strong>', 4),
+      array('<strong>test test</strong>', '<strong>test test</strong>', 3),
+    );
+  }
+
 }
